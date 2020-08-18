1b1217f2bb407ba600b781ae3c6330f5c84239b7
PUSHPENDRA SINGH <pushpendra2010056@gmail.com>
2020-08-18 20:02:20 +0530

    Update settings.py
    update DATABASES in settings.py

--- silverlining/settings.py
+++ silverlining/settings.py
@@ -100,10 +100,21 @@ WSGI_APPLICATION = 'silverlining.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'silverlining',                     
+        'USER': 'silverlining',                     
+        'PASSWORD': 'Welcome@123',              
+        'HOST': 'silverlining.database.windows.net',                     
+        'PORT': '1433',
     }
 }
 
