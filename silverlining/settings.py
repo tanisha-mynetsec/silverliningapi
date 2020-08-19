d3754086561d2ce0977ea3d07649362248f0031b
PUSHPENDRA SINGH <pushpendra2010056@gmail.com>
2020-08-19 16:28:50 +0530

    Update settings.py
    fix

--- silverlining/settings.py
+++ silverlining/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'silverlining.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -118,16 +118,16 @@ WSGI_APPLICATION = 'silverlining.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'silverlining',                     
-        'USER': 'root',                     
-        'PASSWORD': 'silver@123',              
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'silverlining',                     
+#         'USER': 'root',                     
+#         'PASSWORD': 'silver@123',              
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
